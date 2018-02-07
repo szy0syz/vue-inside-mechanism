63af2829d9c63fdb8d0e04b62ca858513420178e
szy0syz <szy0syz@gmail.com>
2018-02-07 22:50:32 +0800

    update ch02 依赖收集

--- vue.js
+++ vue.js
@@ -1,9 +1,3 @@
-// 渲染视图不在cb里操作了
-// function cb(val) {
-//   // 渲染视图
-//   console.log('视图更新了~~')
-// }
-
 // 订阅者
 class Dep {
   constructor () {
@@ -14,10 +8,17 @@ class Dep {
   // 在subs中添加一个Watcher对象
   addSub (sub) {
     this.subs.push(sub)
+    console.log('Dep..this.subs: ',this.subs)
+    console.log('this.subs.length',this.subs.length)
+    // 测试相等不
+    if(this.subs.length>1) {
+      console.log('真相--->', this.subs[0].update === this.subs[1].update)
+    }
   }
 
   //
   notify () {
+    console.dir('kkk ',this.subs)
     this.subs.forEach((sub) => {
       // 这里sub就是一个个wather实例
       sub.update()
@@ -30,6 +31,7 @@ class Watcher {
   constructor () {
     // 在new一个Watcher对象时将该对象赋值给Dep.target，在get中会用到
     Dep.target = this
+    // 原来是这里，这里是个静态属性，你管你new几次，牢牢地只会指向一个地址
   }
 
   // 更新视图的方法
@@ -41,25 +43,25 @@ class Watcher {
 // 暂时先置空
 Dep.target = null
 
-
-
 /* 
 *  定义响应式的对象属性 
 *  流程：将传入的对象，其属性名，值配置一下
 *        然后分别设置get和set
 *        get时会进行依赖收集
 *        set时会触发回调来重新赋值和更新试图
-* 【add】：依赖手机
+* 【add】：依赖收集
 */       
 function defineReactive(obj, key, val) {
-  /* 一个Dep类对象 */
+  /* new一个Dep类对象实例 */
   const dep = new Dep();
 
   Object.defineProperty(obj, key, {
     enumerable: true,   // 属性可枚举
     configurable: true, // 属性可以修改或删除
     get: function reactiveGetter() {
-      /* 将Dep.target（即当前的Watcher对象存入dep的subs中） */
+      /* 将Dep.target（即当前的Watcher对象)存入dep的subs中） */
+      console.log('defineProperty >>>')
+      console.dir(Dep.target)
       dep.addSub(Dep.target)
       return val // 依赖收集
     },
@@ -71,7 +73,8 @@ function defineReactive(obj, key, val) {
   })
 }
 
-/* 【观察者】
+/* 【可观察的】
+* 作用 将传进来的对象的所有属性可观察化
 * 流程
 *   - 判断传入value是空或者非对象就返回
 *   - 遍历对象中所有可遍历属性逐一加入defineReactive函数将属性变为响应式
@@ -115,4 +118,6 @@ let vv2 = new Vue({
 
 // vv._data.test = 'I am a coder11111.' // 视图更新了~~ 
 
+// 按理说应该要更新两次才对，但才更新一次？
 globalObj.name = 'szy999'
+
